3e0146a7f637a6b8edf6d18e80a7e41e85b44999
George Hill <george@zzish.com>
2024-01-27 23:08:02 +0000

    login page with header, h1

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Popover from '$lib/components/ui/popover';
+	import { goto } from '$app/navigation';
+
+</script>
+
+<div class="p-6">
+
+	<div class="mb-3 shadow w-full fixed inset-0 h-fit p-4">
+		<h2 class="text-3xl font-extrabold tracking-tight lg:text-5xl">
+			Accelearn
+		</h2>
+	</div>
+
+    <h1 class="scroll-m-20 text-5xl font-extrabold tracking-tight lg:text-5xl mt-20">
+        Hi, what is your phone number?
+    </h1>
+
+	
+
+    
+</div>
