1c4257c8eb9771cc9fc033e4dc795b38baa0c03d
JitPackJoyride <ajitkrishna99@gmail.com>
2024-01-28 14:48:07 +0000

    fix: remove footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,21 +14,3 @@
 		<slot />
 	</div>
 </div>
-<footer class="">
-	<p>
-		Lack of skills is one of the <a
-			href="https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/622214/Time_for_Change_report_-_An_assessement_of_government_policies_on_social_mobility_1997-2017.pdf">
-			primary drivers
-		</a>
-		of rising inequality in the UK.
-	</p>
-	<p>
-		<a
-			href="https://assets.publishing.service.gov.uk/media/5c4f25a3e5274a493c308f9e/Adult_skills_report_2019.pdf">
-			50% of adults
-		</a>
-		 in the bottom socio-economic class in the UK had participated in no learning in the last three years,
-		compared to 20% in the top group.
-	</p>
-	<p>It's time to change that.</p>
-</footer>
