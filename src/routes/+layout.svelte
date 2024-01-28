29cdf6a6aa72f144540f4eeaf69b6a424d246e2e
JitPackJoyride <ajitkrishna99@gmail.com>
2024-01-28 14:42:45 +0000

    feat: better full screen layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.pcss';
 </script>
 
-<div class="mx-auto min-h-full max-w-screen-md px-5 pb-5 pt-2 antialiased">
+<div class="mx-auto flex min-h-full max-w-screen-md px-5 pb-5 pt-2 antialiased">
 	<div class="fixed inset-0 h-fit bg-background py-2 shadow">
 		<div class="mx-auto max-w-screen-md px-5">
 			<a href="/">
@@ -10,7 +10,7 @@
 			</a>
 		</div>
 	</div>
-	<div class="mt-16 h-[calc(100%-4rem)] scroll-mt-16">
+	<div class="mt-16 min-h-[calc(100dvh-4rem-28px)] flex-1 scroll-mt-16">
 		<slot />
 	</div>
 </div>
